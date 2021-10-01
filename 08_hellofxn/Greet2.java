8aec8b121d28f17fe2b031483d12ce6f44097856
Anjini Katari <anjinikatari@10-20-129-247.dynapool.wireless.nyu.edu>
2021-09-30 22:40:46 -0400

    notes for greet2

--- 08_hellofxn/Greet2.java
+++ 08_hellofxn/Greet2.java
@@ -9,3 +9,17 @@ public class Greet2 {
 		System.out.println(greet("Mrs. Hudson"))
 	}
 }
+
+\*
+Discoveries:
+ * be cautious with c/p -> sometimes weird characters get mixed up with your code, which leads to much heartache
+ * adding on to the above - if there's something wrong with the file/perhaps the computer ask a friend to run the code for you in their terminal (done with a friend from a different class & teacher)
+ * main is really only used for printing the stuff you want the code to print
+
+Unresolved Mysteries 
+ * Why do said weird characters pop up?
+ * Why doesn't the computer warn you of the weird character? Is there a way to check for them?
+ * Is there a more conscice way of writing this code?
+ * how would you randomize the names printed in the hello string?
+
+*/
