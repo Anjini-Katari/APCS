bfadf28e48a30bccd895e33349c8274eb67f6b40
Anjini Katari <anjinikatari@10-20-95-155.dynapool.wireless.nyu.edu>
2021-10-03 22:48:21 -0400

    09 hw

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+//Anjini Katari
+//APCS
+//HW09 -- What Are BigSibs Good For?
+//2021-10-04
+
+public class BigSib {
+	public static String Greet(String name){
+		String hello = "Why, hello there, " + name + ". How do you do?";
+		System.out.println(name);
+		return hello;
+	}
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,24 @@
+\*
+Anjini Katari
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-04
+*/
+
+public class Greet {
+	public static void main(String[] args){
+                BigSib.Greet("Film");
+		BigSib.Greet("Flam");
+		BigSib.Greet("Kazaam");
+	}
+}
+
+\*
+Unresolved Mysteries:
+ * Still slightly confused as to why this works
+ * Does this work if the file/class is in a different dir?
+
+Discoveries:
+ * Clarification on how this works! BigSib.Greet says to look at the class BigSib, and run the method Greet
+ * Methods and Objects are in Python as well! (Ex. Numpy and Tensorflow for ML)
+*/
