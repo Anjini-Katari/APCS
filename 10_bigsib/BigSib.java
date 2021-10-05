c99556f9437a6e9e7b9ac152fa3863dd04d4ccd2
Anjini Katari <anjinikatari@10-20-95-155.dynapool.wireless.nyu.edu>
2021-10-04 22:38:25 -0400

    hw 10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+//Anjini Katari
+//APCS
+//HW10 -- Refactor Big Sib One
+//2021-10-05
+
+public class BigSib {
+	public static String greet(String name){
+		String hello = "Why, hello there, " + name + ". How do you do?";
+		return hello;
+	}
+}
+
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,24 @@
+//Anjini Katari
+//APCS
+//HW10 -- Refactor Big Sib One
+//2021-10-05
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
+
+\*
+Unresolved Mysteries: 
+ * Is this way considered 'more effecient' than how we did this for the last 
+     homework? In other words, should is it better to hace the Systems.out.print in 
+     the file executing the method, or the file of the method being defined?
+
+Discoveries:
+ * In the Big Sib file from the last hw, there was a print statement -> if you keep 
+     that in, the output becomes the name and the greeting statement!
+
+*/
