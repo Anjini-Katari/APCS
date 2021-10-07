edd2b8b1daae6c9064fdc927fbd9e88e7233fa25
Anjini Katari <anjinikatari@10-20-95-155.dynapool.wireless.nyu.edu>
2021-10-06 22:18:38 -0400

    hw 13

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,23 +1,23 @@
 public class Greet {
-    public static void main( String[] args ) {
-        String greeting;
+	public static void main( String[] args ) {
+        	String greeting;
 
-        BigSib richard = new BigSib();
-        richard.setHelloMsg("Word up");
-                greeting = richard.greet("freshman");
-                System.out.println(greeting);
+		BigSib richard = new BigSib();
+		greeting = richard.greet("freshman");
+		System.out.println(greeting);
 
-        BigSib Anjini = new BigSib();
-        Anjini.setHelloMsg("Salutations");
-                greeting = Anjini.greet("Dr. Spaceman");
-                System.out.println(greeting);
+        	BigSib Anjini = new BigSib();
+		greeting = Anjini.greet("Dr. Spaceman");
+		System.out.println(greeting);
 
-                BigSib Ruby = new BigSib();
-                Ruby.setHelloMsg("Hey ya");
-                greeting = Ruby.greet("Kong Fooey");
-                System.out.println(greeting);
+		BigSib Ruby = new BigSib();
+		greeting = Ruby.greet("Kong Fooey");
+		System.out.println(greeting);
 
-                BigSib Watson = new BigSib();
-                Watson.setHelloMsg("Sup");
-                greeting = Watson.greet("mom");
-                System.out.println(greeting);
+		BigSib Watson = new BigSib();
+		greeting = Watson.greet("mom");
+		System.out.println(greeting);
+
+	}
+}
+
