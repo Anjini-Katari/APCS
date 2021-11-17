bc045a059e6dc330e90a06880e5dd338353ed24e
Anjini Katari <akatari30@stuy.edu>
2021-11-17 16:48:36 -0500

    hw38 init

---
+++ 38_slots/classTests.java
@@ -0,0 +1,12 @@
+public class classTest {
+	public static boolean integer() {
+		Integer x = newInteger(3);
+		Integer y = 3;
+		x == y;
+		x.equals(y);
+	}
+
+	public static void main {
+		System.out.println(integer);
+	}
+}
