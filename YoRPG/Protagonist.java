4dff3ad6c249ccd13dea61b6c2abd6a16176ec53
Anjini Katari <akatari30@stuy.edu>
2021-11-22 21:29:55 -0500

    update YoRPG

---
+++ YoRPG/Protagonist.java
@@ -0,0 +1,23 @@
+public class Protagonist {
+
+	public String getName(String name) {
+		String protagName = name;
+	}
+
+	public boolean isAlive() {
+	
+	}
+
+	public <ret type> specialize() {
+	
+	}
+	
+	public <ret type> normalize() {
+	
+	}
+	
+	public <ret type> attack() {
+	
+	}
+		
+} 
