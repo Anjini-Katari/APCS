f53fce736af9b94cb6a816763a0162b8dfce40ca
Anjini Katari <akatari30@stuy.edu>
2021-12-07 20:45:52 -0500

    hw 44 classwork

--- otherTests/arraylentest.java
+++ otherTests/arraylentest.java
@@ -1,18 +1,20 @@
-'public class arraylentest {
-	public static singArray ( int[] a ) {
+public class arraylentest {
+	public static String singArray ( int[] a ) {
   		for (int i = 0; i < a.length; i += 1) {
 				System.out.println(a[i]);
 			}
+		return "yay";
   	}
 
 	public static String print1( int[][] b ) {
 		System.out.println(b.length);
+		return "whee";
   	}
 
 	public static void main(String[] args) {
 		int [][] m2 = { {2,4,6}, {3,5,7} };
 		int [] m = {2, 4, 5};
 		singArray(m);
-		print1()
+		print1(m2);
 	}
  }
