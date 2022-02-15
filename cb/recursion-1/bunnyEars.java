f2a700eb773be083cdf171bd3eacf9ed0365a0c8
Anjini Katari <anjinikatari@10-20-3-129.dynapool.wireless.nyu.edu>
2022-02-14 23:11:42 -0500

    hw

--- cb/recursion-1/bunnyEars.java
+++ cb/recursion-1/bunnyEars.java
@@ -1,3 +1,10 @@
+/*
+Red Apples: Gloria Lee and Anjini Katari (with duckies Flounder and Watson)
+APCS
+HW64: Revisitation
+2022-02-15
+time spent: 1.5h
+*/
 public class bunnyEars{
 
 	public int bunnyEars(int bunnies) {
