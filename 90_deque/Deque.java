4df8780f580c4bec55db8dfef9f564a37b858408
Anjini Katari <anjinikatari@10-20-172-243.dynapool.wireless.nyu.edu>
2022-04-13 07:29:43 -0400

    hw 90

---
+++ 90_deque/Deque.java
@@ -0,0 +1,30 @@
+//Under Pressure: Julia Kozak, Yat Long Chan, Anjini Katari
+//APCS pd08
+//HW90 - Swabbing the Deque
+//2022-04-12T
+//time spent: 0.4 hours
+
+public interface Deque<T> {
+    
+    public T peekFirst();
+
+    public T peekLast();
+
+    public boolean isEmpty();
+
+    public void addFirst(T x);
+
+    public void addLast(T x);
+
+    public T removeFirst();
+
+    public T removeLast();
+
+    public T peek();
+
+    public T remove();
+
+    public boolean add(T x);
+
+    public int size();
+}
---
+++ 90_deque/LLDeque.java
@@ -0,0 +1,78 @@
+//Under Pressure: Julia Kozak, Yat Long Chan, Anjini Katari
+//APCS pd08
+//HW90 - Swabbing the Deque
+//2022-04-12T
+//time spent: 0.4 hours
+
+
+import java.util.LinkedList;
+
+public class LLDeque<T> implements Deque<T> {
+    private LinkedList<T> _dequeue;
+
+    public LLDeque() {
+        _dequeue = new LinkedList<T>();
+    }
+
+    public T peekFirst() {
+        return _dequeue.peekFirst();
+    }
+
+    public T peekLast() {
+        return _dequeue.peekLast();
+    }
+
+    public boolean isEmpty() {
+        return _dequeue.size() <= 0;
+    }
+
+    public void addFirst(T x) {
+        _dequeue.addFirst(x);
+    }
+
+    public void addLast(T x) {
+        _dequeue.addLast(x);        
+    }
+
+    public T removeFirst() {
+        return _dequeue.removeFirst();
+    }
+    
+    public T removeLast() {
+        return _dequeue.removeLast();
+    }
+
+    public T peek() {
+        return _dequeue.peek();        
+    }
+
+    public boolean add(T x) {
+        return _dequeue.add(x);
+    }
+
+    public T remove() {
+        return _dequeue.remove();
+    }
+
+    public int size() {
+        return _dequeue.size();
+    }
+
+    public static void main(String[] args) {
+        Deque dq = new LLDeque();
+        dq.add("x");
+        dq.addFirst(1);
+        dq.addLast(2.3);
+        System.out.println(dq.peekFirst()); // 1
+        System.out.println(dq.peek()); // 1
+        System.out.println(dq.peekLast()); // 2.3
+        System.out.println(dq.removeLast()); // 2.3
+
+        while (!dq.isEmpty()) {
+            System.out.println(dq.removeFirst()); // 1 x 
+        }
+
+        // System.out.println(dq.remove()); //exception
+
+    }
+}
