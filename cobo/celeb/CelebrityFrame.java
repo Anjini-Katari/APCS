c43a7413af511e85de5fc30a5f0e656cdd84c94d
Anjini Katari <akatari30@stuy.edu>
2022-04-27 13:24:17 -0400

    class stuff

--- cobo/celeb/CelebrityFrame.java
+++ cobo/celeb/CelebrityFrame.java
@@ -1,3 +1,10 @@
+/*
+TNPG Chocolate Freds: Anjini Katari, Yuki Feng, Hamim Seam
+APCS pd08
+Lab09: Some Folks Call It Charades
+2022-04-27
+Time spent: 3.0 hrs
+*/
 import java.awt.CardLayout;
 
 import javax.swing.JFrame;
@@ -10,29 +17,29 @@ import javax.swing.JPanel;
  */
 public class CelebrityFrame extends JFrame
 {
-	//Data members for the JFrame subclass instance 
-	
+	//Data members for the JFrame subclass instance
+
 	/**
 	 * The instance of the panel used for playing the game.
 	 */
 	private CelebrityPanel gamePanel;
-	
+
 	/**
 	 * Blank panel used for the CardLayout switching screens feature.
 	 */
 	private JPanel panelCards;
-	
+
 	/**
 	 * The instance variable used to add a celebrity to the game.
 	 */
 	private StartPanel startPanel;
-	
+
 	/**
 	 * A reference to the CelebrityGame instance to allow for minimized coupling in the object structure.
 	 */
 	private CelebrityGame controller;
-	
-	
+
+
 	/**
 	 * Builds an instance of the CelebFrame with a reference to the CelebrityGame instance.
 	 * @param controller A reference to a CelebrityGame to share with the CelebPanel instance.
@@ -41,30 +48,29 @@ public class CelebrityFrame extends JFrame
 	{
 		//The first line of any subclass should ALWAYS be a correct call to the super constructor.
 		super();
+		this.controller = controllerRef;
+		this.panelCards = new JPanel(new CardLayout());
+		this.gamePanel = new CelebrityPanel(controller);
+		this.startPanel = new StartPanel(controller);
 		setupFrame();
 	}
-	
+
 	/**
 	 * Configures the JFrame window subclass to add the panel and set size based information.
 	 */
 	private void setupFrame()
 	{
-		JFrame frame = new JFrame("Celeb");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
-
-		StartPanel start = new StartPanel(controller);
+		panelCard.add
 
 	}
-	
+
 	/**
 	 * Swaps the screen between the existing panels based on the String parameter.
 	 * @param screen The name of the screen to open.
 	 */
 	public void replaceScreen(String screen)
 	{
-		StartPanel start = new StartPanel(controller);
+
 	}
-	
+
 }
